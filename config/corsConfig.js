08b885f73db5843dc558ee273501487d3edb0748
jorboare <jordi.b.arevalo@gmail.com>
2024-05-15 10:43:56 +0200

    cors config

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -3,12 +3,11 @@ const whitelist = [""];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    callback(null, true);
-    // if (whitelist.indexOf(origin) !== -1 || !origin) {
-    //   callback(null, true);
-    // } else {
-    //   callback(new Error("Not allowed by CORS"));
-    // }
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
   },
 };
 
