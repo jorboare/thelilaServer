749523560440cbb8ea01a8f4d14ec9c43d61229d
jorboare <jordi.b.arevalo@gmail.com>
2024-05-15 10:08:27 +0200

    Server initial config

---
+++ config/corsConfig.js
@@ -0,0 +1,14 @@
+// corsConfig.js
+const whitelist = [""];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+module.exports = corsOptions;
